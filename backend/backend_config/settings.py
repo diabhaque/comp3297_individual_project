fec8fada072e32611d1345112cee02ea20612319
Diab Haque <diabhaque@gmail.com>
2021-03-28 22:20:16 +0800

    Server: Fingers Crossed

--- backend/backend_config/settings.py
+++ backend/backend_config/settings.py
@@ -25,16 +25,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('CHP_SECRET_KEY')
+# SECRET_KEY = env('CHP_SECRET_KEY')
+SECRET_KEY = 'abcs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('CHP_DEBUG', default=False)
+# DEBUG = env.bool('CHP_DEBUG', default=False)
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    'cov-retrieve.herokuapp.com',
-    'localhost',
-    '127.0.0.1'
-]
+# ALLOWED_HOSTS = [
+#     'cov-retrieve.herokuapp.com',
+#     'localhost',
+#     '127.0.0.1'
+# ]
 
 
 # Application definition
@@ -99,15 +101,15 @@ WSGI_APPLICATION = 'backend_config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default':{
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'cov_retrieve',
-    #     'USER': 'diab_swe',
-    #     'PASSWORD': 'swe',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432'
-    # }
-    'default': env.dj_db_url('DATABASE_URL')
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cov_retrieve',
+        'USER': 'diab_swe',
+        'PASSWORD': 'swe',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+    # 'default': env.dj_db_url('DATABASE_URL')
 }
 
 
