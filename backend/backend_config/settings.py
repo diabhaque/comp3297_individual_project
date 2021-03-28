3798a7621c8c9dfddee067265823d5a8f249f5ce
Diab Haque <diabhaque@gmail.com>
2021-03-28 22:31:31 +0800

    djshfalsj

--- backend/backend_config/settings.py
+++ backend/backend_config/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('CHP_SECRET_KEY')
-# SECRET_KEY = 'abcs'heri
+# SECRET_KEY = env('CHP_SECRET_KEY')
+SECRET_KEY = 'abcs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('CHP_DEBUG', default=False)
-# DEBUG = True
+# DEBUG = env.bool('CHP_DEBUG', default=False)
+DEBUG = False
 
 # ALLOWED_HOSTS = [
 #     'cov-retrieve.herokuapp.com',
